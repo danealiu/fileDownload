d228ab62e5e6aa650611fd72002ec04d3dea0290
jasontao <1648500@qq.com>
2016-06-21 20:49:28 +0800

    fix ie and chrome bug.

--- src/fileDownload.js
+++ src/fileDownload.js
@@ -13,8 +13,7 @@
 
         _fD.prototype = {
             'downloadByIframe': function() {
-                var isIE = fD.getBrowser().indexOf('IE') === -1;
-                if (!isIE) new Error('only IE support downloadByIframe');
+                if (!fD.getBrowser() === 'IE') new Error('only IE support downloadByIframe');
 
                 var d = win.document;
                     var iframeEl= document.createElement('iframe');
@@ -25,8 +24,7 @@
                 d.body.removeChild(iframeEl);
             },
             'downloadByCreateObjectURL': function() {
-                var isIE = fD.getBrowser().indexOf('IE') === -1;
-                if (isIE) new Error('downloadByCreateObjectURL not support IE');
+                if (fD.getBrowser() !== 'IE') new Error('downloadByCreateObjectURL not support IE');
                 var d = document;
                 var event = d.createEvent('MouseEvents');
                 var aEl = document.createElement("a");
@@ -53,37 +51,10 @@
         var isIE = userAgent.indexOf('compatible') > -1 && userAgent.indexOf("MSIE") > -1 && !isOpera;
         var isFF = userAgent.indexOf('Firefox') > -1;
         var isChrome = userAgent.indexOf('Chrome') > -1;
-        if (isIE) {
-            var IE5 = IE55 = IE6 = IE7 = IE8 = false;
-            var reIE = new RegExp("MSIE (\\d+\\.\\d+);");
-            reIE.test(userAgent);
-            var fIEVersion = parseFloat(RegExp["$1"]);
-            IE55 = fIEVersion == 5.5;
-            IE6 = fIEVersion == 6.0;
-            IE7 = fIEVersion == 7.0;
-            IE8 = fIEVersion == 8.0;
-            if (IE55) {
-                return 'IE5';
-            }
-            if (IE6) {
-                return 'IE6';
-            }
-            if (IE7) {
-                return 'IE7';
-            }
-            if (IE8) {
-                return 'IE8';
-            }
-        }
-        if (isFF) {
-            return 'FF';
-        }
-        if (isOpera) {
-            return 'Opera';
-        }
-        if (isChrome) {
-            return 'Chrome';
-        }
+        if (isIE)     return 'IE';
+        if (isFF)     return 'FF';
+        if (isOpera)  return 'Opera';
+        if (isChrome) return 'Chrome';
     };
 
     if (typeof module != 'undefined' && module.exports && this.module !== module) { module.exports = fD }
